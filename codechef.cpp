e085d69600ceb5a0a6f45f8aec4e366d05b2c80c
rachit-geek <55797045+rachit-geek@users.noreply.github.com>
2020-12-03 21:44:54 +0530

    code 3

---
+++ codechef.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+    long long int x,y;
+    cin>>x>>y;
+    long long int k=x-y;
+    if(k==1)
+    {
+        cout<<"NO"<<endl;
+    }
+    else
+    {
+        cout<<"YES"<<endl;
+    }
+    }
+
+}
