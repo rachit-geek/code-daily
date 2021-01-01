35b7a24fe334758f47d6aedface44b0dfa55b311
rachit-geek <55797045+rachit-geek@users.noreply.github.com>
2021-01-01 16:56:34 +0530

    Create lastmin.cpp

---
+++ lastmin.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;int arr[n];
+        set<int> s;
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int i=0;i<n;i++)
+        {
+            if(s.count(arr[i]))
+            {
+                arr[i]++;
+            }
+            s.insert(arr[i]);
+        }
+
+        cout<<s.size()<<endl;
+    }
+
+}
