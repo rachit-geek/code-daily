7fa8dcc43880b97d2fb8fb13dae1a3e1e878012e
rachit-geek <55797045+rachit-geek@users.noreply.github.com>
2021-01-01 16:56:03 +0530

    Create delima.cpp

---
+++ delima.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;int arr[n];
+        set<int> s;
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                    s.insert(abs(arr[i]-arr[j]));
+            }
+        }
+
+        cout<<s.size()<<endl;
+    }
+
+}
