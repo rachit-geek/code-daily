96e0f8f4b685b3e052abc63aeb610922246dc6b6
rachit-geek <55797045+rachit-geek@users.noreply.github.com>
2021-01-13 22:25:23 +0530

    Create str.cpp

---
+++ leetcode/str.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+        {
+            return 0;
+        }
+        int n=haystack.length()- needle.length()+1;
+        for(int i=0;i<n;i++)
+        {
+            int j=0,k=i;
+            while(j<needle.length() and needle[j]==haystack[k])
+            {
+                j++;k++;
+            }
+            if(j==needle.length())
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
