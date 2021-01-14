acdfe7e1930337242c5feea23ddb87e4ce170959
rachit-geek <55797045+rachit-geek@users.noreply.github.com>
2021-01-14 23:14:53 +0530

    Create number.cpp

---
+++ codeforces/number.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;cin>>t;
+while(t--)
+{
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+
+    }
+    sort(arr,arr+n);
+    int flag=0;
+
+    for(int i=0;i<n;i++)
+    {
+
+        if(arr[i]<=k)
+        {
+            flag++;
+
+        }
+    }
+    if(flag==n)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        if(arr[0]+arr[1]<=k and n>=2)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+}
+}
