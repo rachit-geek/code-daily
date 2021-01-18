f4088d49ddd1988ab2db383c09bd86c0f7fb5e38
rachit-geek <55797045+rachit-geek@users.noreply.github.com>
2021-01-18 23:39:16 +0530

    Update lcsrec.cpp

--- dp/lcsrec.cpp
+++ dp/lcsrec.cpp
@@ -1,1 +1,19 @@
-asdfghjkkjhgfdsdfgh
+//asdfghjkkjhgfdsdfgh
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcsrec(string X,string Y, int n, int m)
+{
+  if(n==0 or m==0)
+  {
+    return 0;
+  }
+  if(X[n-1]==Y[m-1])
+  {
+    return 1+lcsrec(X,Y,n-1,m-1);
+  }
+  else
+  {
+    return max(lcsrec(X,Y,n,m-1),lcsrec(X,Y,n-1,m));
+  }
+}
