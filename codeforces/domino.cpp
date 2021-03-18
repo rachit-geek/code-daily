de713e5828278e3b92f32665f8f467ad1d121dbd
goyalrachit02@gmail.com <goyalrachit02@gmail.com>
2021-03-19 02:12:49 +0530

    solving contest

---
+++ codeforces/domino.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;while(t--)
+    {
+       int n,k1,k2;
+       cin>>n>>k1>>k2;
+       int w,b;
+       cin>>w>>b;
+       int white=k1+k2;
+       int black=2*n-white;
+       if(2*w<=white and 2*b<=black)
+       {cout<<"YES"<<endl;
+       }
+       else
+       {cout<<"NO"<<endl;}
+    }
+}
+
