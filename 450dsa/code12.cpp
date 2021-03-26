4a3432ad3d53a53206196573f7a9ba54b02fea1f
goyalrachit02@gmail.com <goyalrachit02@gmail.com>
2021-03-26 23:41:55 +0530

    adding files

---
+++ 450dsa/code12.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i,j,maxp=0;int cp;
+        for(int i=0;i<prices.size();i++)
+        {
+            cp=prices[i];
+            for(j=i+1;j<prices.size();j++)
+            {
+                if(prices[j]-cp>maxp)
+                {
+                    maxp=prices[j]-cp;
+                }
+            }
+        }
+        return maxp;
+        
+    }
+};
