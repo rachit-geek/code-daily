ec8593901b1c5333301c7f915e29b372ae6b7c1f
goyalrachit02@gmail.com <goyalrachit02@gmail.com>
2021-05-19 01:19:29 +0530

    adding file

---
+++ 450dsa/trees/prob5.cpp
@@ -0,0 +1,34 @@
+vector<int> leftView(Node *root)
+{
+    vector<int> ans;
+    if(root==NULL)
+    {
+        return ans;
+    }
+    queue<Node*> q;
+    q.push(root);
+    Node* temp=NULL;
+    while(!q.empty())
+    {
+        int n=q.size();
+        for(int i=1;i<=n;i++)
+        {
+            temp=q.front();
+            q.pop();
+            if(i==1)
+            {
+                ans.push_back(temp->data);
+            }
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+    return ans;
+   // Your code here
+}
