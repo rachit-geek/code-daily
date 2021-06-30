0fc8e7219b342d0c8a2a6330548ecd2e8f524584
Rachit Goyal <55797045+rachit-geek@users.noreply.github.com>
2021-06-30 23:42:04 +0530

    Update prob1.cpp

--- 450dsa/Arrays/prob1.cpp
+++ 450dsa/Arrays/prob1.cpp
@@ -1,7 +1,7 @@
  // PROGRAM TO REVERSE AN ARRAY  
 #include<bits/stdc++.h>     
 using namespace std;
-void revarr(int arr[],int n)
+void revarr(int arr[],int n) 
 { 
     int start=0;int end=n-1; 
     int temp;  
